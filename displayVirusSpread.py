17f8ee99181394a13d58f895a89999941b816060
jbllona <jesse.llona@gmail.com>
2018-05-14 18:00:50 -0700

    added method to build star graph from a file, and a field to dataToDisplay to pass which nodes are infected at the begining

--- displayVirusSpread.py
+++ displayVirusSpread.py
@@ -2,9 +2,23 @@ import numpy as N
 import matplotlib as plt
 from enum import Enum
 
+def drawGraphFromFile(fileName):
+    retVal = []
+    with open(fileName, 'r') as file:
+        for line in file:
+            if line.find('\n') != -1:
+                retVal.append(line[:line.find('\n')].split(" "))
+            else:
+                retVal.append(line.split(" "))
+    return retVal
+
+STAR_GRAPH = drawGraphFromFile('starGraph.txt')
+
 class graphType(Enum):
+    NONE = 0
     STAR = 1
     TREE = 2
 
 class dataToDisplay:
-    graphType =
+    typeOfGraph = graphType.NONE
+    startNodes = []
